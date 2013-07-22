07652742c94fde2c6b2e437ca03c56be59486fd7
radproject <radhouanef@gmail.com>
2013-07-22 12:33:31 +0100

    added gender page (blanc)

---
+++ js/gender.js
@@ -0,0 +1,82 @@
+var gridView = $("#grid-control").length > 0;
+var $bar = $(".bar");
+if ($bar.length) {
+var $links = $(".bar-section a");
+$links.on('click', function() {
+if (!$(this).hasClass("on")) {
+var href = $(this).attr('href').replace(/^#/, ''),
+option = $.deparam(href, true);
+//alert(href);
+//gender switching
+$.bbq.pushState(option);
+}
+return false;
+});
+} 
+$('.view').live('click', function(){
+	$('.view').each(function(){
+		$(this).removeClass('on')
+                
+	});
+	$(this).addClass('on');
+});
+var $typeLinks = $(".bar-section-view a");
+var $catLinks = $(".bar-section-cat a");
+$(window).bind('hashchange', function(event) {
+	var hashOptions = $.deparam.fragment();
+	if (hashOptions.gender == null) hashOptions.gender = "all";
+	if (hashOptions.layer == null) hashOptions.layer = "vote";
+	var filter = "";
+	for (var opt in hashOptions) {
+		if (hashOptions[opt] != "all") {
+			filter += "." + hashOptions[opt];
+		}
+	}
+	var $typeLink = $typeLinks.filter("[href='#gender=" + hashOptions.gender +"']");
+	$typeLinks.removeClass("on");
+	$typeLink.addClass("on");
+	var $catLink = $catLinks.filter("[href='#layer=" + hashOptions.layer +"']");
+	var removeClass = $catLinks.filter(".on").length > 0 ? $catLinks.filter(".on").attr("href").replace("#config=","") : "sunni";
+	$(".categories .menu").html($catLink.html()).removeClass(removeClass).addClass(hashOptions.category).append("<em></em>");
+	$catLinks.removeClass("on");
+	$catLink.addClass("on");
+        changeMap(hashOptions);
+	})
+.trigger('hashchange');
+var $catMenu = $(".bar .bar-section-cat"), timeOut;
+if ($catMenu.length) {
+var $cats = $catMenu.find("ul");
+$catMenu.hover(function() {
+clearTimeout(timeOut);
+$cats.css("display", "block");
+}, function() {
+clearTimeout(timeOut);
+timeOut = setTimeout(function() {
+$cats.css("display", "none");
+}, 500);
+});
+}
+var $about = $(".about");
+var $aboutBox = $("#about-box");
+if ($about.length && $aboutBox.length) {
+$about.colorbox({
+inline:true,
+width:"780px",
+"left":"220px",
+href:"#about-box",
+scrolling:false,
+onClosed: function() {
+if ($.cookie("returning") == null) {
+$.cookie("returning", true);
+}
+$('.view-map').trigger('click');
+}
+});
+/*if ($.cookie("returning") == null) {
+$about.colorbox({open:true});
+}*/
+$("#about-box .close").on('click', function() {
+$.colorbox.close();
+return false;
+});
+} 
---
+++ js/mapgender.js
@@ -0,0 +1,163 @@
+var layers = [{
+      name: 'sunni0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.sunni0509'),
+          L.mapbox.gridLayer('tunisia.sunni0509')])
+  },
+  {
+      name: 'sunni0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.sunni0912'),
+          L.mapbox.gridLayer('tunisia.sunni0912')])
+  },
+  {
+      name: 'alawite0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.alawite0509'),
+          L.mapbox.gridLayer('tunisia.alawite0509')])
+  },
+  {
+      name: 'alawite0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.alawite0912'),
+          L.mapbox.gridLayer('tunisia.alawite0912')])
+  },
+  {
+      name: 'shia0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.shia0509'),
+          L.mapbox.gridLayer('tunisia.shia0509')])
+  },
+  {
+      name: 'shia0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.shia0912'),
+          L.mapbox.gridLayer('tunisia.shia0912')])
+  },
+  {
+      name: 'druze0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.druze0509'),
+          L.mapbox.gridLayer('tunisia.druze0509')])
+  },
+  {
+      name: 'druze0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.druze0912'),
+          L.mapbox.gridLayer('tunisia.druze0912')])
+  },
+  {
+      name: 'grkorth0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.grkorth0509'),
+          L.mapbox.gridLayer('tunisia.grkorth0509')])
+  },
+  {
+      name: 'grkorth0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.grkorth0912'),
+          L.mapbox.gridLayer('tunisia.grkorth0912')])
+  },
+  {
+      name: 'maronite0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.maronite0509'),
+          L.mapbox.gridLayer('tunisia.maronite0509')])
+  },
+  {
+      name: 'maronite0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.maronite0912'),
+          L.mapbox.gridLayer('tunisia.maronite0912')])
+  },
+  {
+      name: 'evang0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.evang0509'),
+          L.mapbox.gridLayer('tunisia.evang0509')])
+  },
+  {
+      name: 'evang0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.evang0912'),
+          L.mapbox.gridLayer('tunisia.evang0912')])
+  },
+  {
+      name: 'minorities0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.minorities0509'),
+          L.mapbox.gridLayer('tunisia.minorities0509')])
+  },
+  {
+      name: 'minorities0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.minorities0912'),
+          L.mapbox.gridLayer('tunisia.minorities0912')])
+  },
+  {
+      name: 'armorth0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.armorth0509'),
+          L.mapbox.gridLayer('tunisia.armorth0509')])
+  },
+  {
+      name: 'armorth0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.armorth0912'),
+          L.mapbox.gridLayer('tunisia.armorth0912')])
+  },
+  {
+      name: 'armcth0509',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.armcth0509'),
+          L.mapbox.gridLayer('tunisia.armcth0509')])
+  },
+  {
+      name: 'armcth0912',
+      layer: L.layerGroup([
+          L.mapbox.tileLayer('tunisia.armcth0912'),
+          L.mapbox.gridLayer('tunisia.armcth0912')])
+  }];
+var map = L.mapbox.map('map')
+    .setView([33.9274, 35], 8)
+    .addLayer(L.mapbox.tileLayer('tunisia.map-axs0unb8'));
+   
+
+function changeMap(options){
+  // options.date=59 || options.date=912
+
+  if(options.date==59)
+  { 
+    var mapname= options.conf + '0509';
+
+  }
+  else
+  {
+   var mapname= options.conf + '0912';
+  } 
+
+  $.each(layers, function(i, layer) {
+   //going through layers comparing the name to the map name we just made , if found remove the previous one and add the new one 
+  if (layer.name==mapname){
+   console.log ('found layer at index' +i);
+   //removing old layer
+  //map.ease.location({ lat: , lon:  }).zoom(6).optimal();
+ $.each(layers, function(i, l) {
+                    if (l.layer !== layer.layer && map.hasLayer(l.layer)) {
+                        map.removeLayer(l.layer);
+                    }
+                });
+   var gridControl;
+    
+       
+    layer.control = L.mapbox.gridControl(layer.layer.getLayers()[1]);
+    if (gridControl) map.removeControl(gridControl);
+        layer.layer.addTo(map);
+        
+    gridControl = layer.control.addTo(map);
+   }
+
+  });
+
+map.setZoom(8);
+}           
